8da38ff6870825876bc99232aea8c1c24595dd20
tomera <tomera@wix.com>
2021-06-09 11:42:39 +0300

    added minimizing algorithems

---
+++ test_minimizer.py
@@ -0,0 +1,11 @@
+from numpy.core.fromnumeric import reshape
+from minimize_longest_path import basicMinimise, priorityMinimise, priorityLogMinimise
+import numpy as np
+n =10
+alpha = 0.5
+weights = np.array([ i + 0.0 for i in range(n*n)]).reshape(n,n)
+print(weights)
+
+print(basicMinimise(weights, alpha,True))
+print(priorityMinimise(weights, alpha,True))
+print(priorityLogMinimise(weights, alpha,True))
